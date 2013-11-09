a613c2f4ffed7911e02fb5d40739153a7de208c5
lechatpito <lechatpito@gmail.com>
2013-11-09 08:43:25 +0100

    refactoring projcet structure

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ from distutils.core import setup
 setup(name='names',
       version='0.1',
       description='Random name and address generator',
-      licence='Apache 2'
+      license='Apache 2',
       author='Francois Scharffe',
       author_email='francois.scharffe@lirmm.fr',
       url='http://github.com/lechatpito/Names',
-      py_modules=['names/names']
+      py_modules=['src/names/names'],
+      packages=['names'],
+      package_dir={'names': 'src/names'},
+      package_data={'names': ['data/*.txt']},
      )
